d749a5f4c68fb64fbcb6b7f8e2c14457442bf18a
colaziest2-ops <colaziest2@gmail.com>
2026-06-09 11:59:57 +0200

    Update craco.config.js

--- app/frontend/craco.config.js
+++ app/frontend/craco.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 
 module.exports = {
   webpack: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.alias = {
+        ...webpackConfig.resolve.alias,
+        '@': path.resolve(__dirname, 'src'),
+      };
+      return webpackConfig;
     },
   },
   style: {
